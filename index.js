6d55ed32b6dbbb07702683c79aebb5e825f51a81
Malvinder Singh <mdhawan@rehabs.com>
2023-10-12 18:28:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3500;
+
+const usersList = [
+    {
+        id: '1',
+        name: 'Malvinder Singh',
+        email: 'malvinder@example.com',
+        profilePic:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShC7CDrniEZDUN1pO49xLMm1qPd_zd3smFdug0d0mk-_ZoDP40Hj8L5wKimQVCOeDSsr8&usqp=CAU',
+    },
+    {
+        id: '2',
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        profilePic:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0YtOC-DWcKhaIbwJDRuAlgKJKugPwp5dfhKKgOJf_UDtKQdOeZq9CQEetxDF1jmntumU&usqp=CAU',
+    },
+    {
+        id: '3',
+        name: 'Jane Doe',
+        email: 'janedoe@example.com',
+        profilePic:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb3Ghorto2A4X8WIVql4QWhKglZ-aILaz1yhll8Z0heuRnyKf2dunZEoBM47nl640wJJc&usqp=CAU',
+    },
+    {
+        id: '4',
+        name: 'Dummy User',
+        email: 'dummy@example.com',
+        profilePic:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa7dk4pYpCsGPoDzRZGiFbP5fqahoARTZyIHDYanvXdotw08fE4XoVyrHwF-FsOLImQsY&usqp=CAU',
+    },
+];
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+    }),
+);
+
+app.get('/', (req, res) => {
+    res.json({status: 'success', msg: 'success', data: {}});
+});
+
+app.get('/users', (req, res) => {
+    res.json({status: 'success', data: usersList});
+});
+
+app.post('/users', (req, res) => {
+    const {body} = req;
+    usersList.push({...body});
+    res.json({status: 'success', data: usersList});
+});
+
+app.put('/users', (req, res) => {
+    res.json({status: 'success', data: usersList});
+});
+
+app.delete('/users/:id', (req, res) => {
+    const {id} = req?.params;
+    const itemIndex = usersList?.findIndex((item) => item?.id == id);
+    const updatedList = [...usersList];
+    if (itemIndex > -1) {
+        updatedList.splice(itemIndex, 1);
+    }
+
+    res.json({status: 'success', data: updatedList});
+});
+
+app.listen(port, () => {
+    console.log(`SERVER started, listening to port: ${port}`);
+});
